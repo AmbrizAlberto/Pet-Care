f3e951937ed0f19428956fbb59ad23742d05a537
Francisco Rodriguez <chillguy27@outlook.com>
2024-10-12 12:42:06 -0600

    routes moved to Destination file

--- app/src/main/java/com/example/spike/presentation/navigation/Destination.kt
+++ app/src/main/java/com/example/spike/presentation/navigation/Destination.kt
@@ -8,4 +8,10 @@ sealed class Destination(
 ) {
     data object VetList: Destination("home")
     data object Profile: Destination("profile")
+    data object Login: Destination("login")
+    data object Register: Destination("register")
+    data object UserRegister: Destination("user_register")
+    data object Confirmation: Destination("confirmation")
+    data object VetRegister: Destination("vet_register")
+
 }
