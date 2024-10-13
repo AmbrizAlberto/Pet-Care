64ce4264d26af15f8ca974c305efaeb4fb122062
Estefaniax <csamano@ucol.mx>
2024-10-12 20:44:07 -0600

    Add layout to Admin Clients Screen

--- app/src/main/java/com/example/spike/presentation/ui/adminviewsScreen/AdminClientsScreen.kt
+++ app/src/main/java/com/example/spike/presentation/ui/adminviewsScreen/AdminClientsScreen.kt
@@ -1,38 +1,46 @@
+
 package com.example.spike.presentation.ui.adminviewsScreen
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import com.example.spike.presentation.ui.adminviewsScreen.components.BaseLayoutAdminScreen
+import com.example.spike.R
 
 
 
 @Composable
-fun ClientsScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFF2B3340)),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Spacer(modifier = Modifier.height(30.dp))
-    }}
-
-@Preview(showBackground = true)
-@Composable
-fun ClientsScreenPreview() {
-    ClientsScreen()
+fun AdminClientsScreen() {
+    BaseLayoutAdminScreen(title = "Administrar Clientes") {
+        Spacer(modifier = Modifier.height(20.dp))
+    }
 }
 
 
 
 
+@Preview(showBackground = true)
+@Composable
+fun AdminClientsScreenPreview() {
+    AdminClientsScreen()
 
 
+}
