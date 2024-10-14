79e85200882258c9e3b4dd0825dff8d3ea2b2280
Francisco Rodriguez <chillguy27@outlook.com>
2024-10-14 07:33:36 -0600

    appointment screen upload

---
+++ app/src/main/java/com/example/spike/data/model/Appointment.kt
@@ -0,0 +1,9 @@
+package com.example.spike.data.model
+
+data class Appointment (
+    val vetName: String,
+    val placeName: String,
+    val location: String,
+    val date: String,
+    val time: String,
+)
