a77971c553bd63955265690d37b446b0e46fb940
AmbrizAlberto <albertpoambez@gmail.com>
2024-10-14 10:40:26 -0600

    feat: actionbar colors updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
 }
