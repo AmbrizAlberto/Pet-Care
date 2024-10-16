a402e6dfb23b57e54a8ac13f151765f1332cd869
Estefaniax <csamano@ucol.mx>
2024-10-16 10:52:59 -0600

    Corrections in Base Layout Admin Screen

--- app/src/main/java/com/example/spike/presentation/ui/adminviewsScreen/components/BaseLayoutAdminScreen.kt
+++ app/src/main/java/com/example/spike/presentation/ui/adminviewsScreen/components/BaseLayoutAdminScreen.kt
@@ -1,5 +1,6 @@
 package com.example.spike.presentation.ui.adminviewsScreen.components
 
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
