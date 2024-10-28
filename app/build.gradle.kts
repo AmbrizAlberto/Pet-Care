21f2b33698f369d680aae72af3c0ccd54477ce85
arielr <arosas7@ucol.mx>
2024-10-28 16:58:33 -0600

    register screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.constraintlayout.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -69,6 +70,10 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
+    implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+
 
     // Retrofit y Gson
     implementation(libs.retrofit)
