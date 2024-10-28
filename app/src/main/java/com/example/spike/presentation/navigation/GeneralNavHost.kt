1229b4ee036e9e81ddebc620a96213d0edefce54
arielr <arosas7@ucol.mx>
2024-10-28 16:53:23 -0600

    new destination for user, vet and pet

--- app/src/main/java/com/example/spike/presentation/navigation/GeneralNavHost.kt
+++ app/src/main/java/com/example/spike/presentation/navigation/GeneralNavHost.kt
@@ -3,14 +3,23 @@ package com.example.spike.presentation.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.spike.presentation.ui.shared.screenLogin.LoginScreen
 import com.example.spike.presentation.ui.shared.screenRegister.ConfirmationScreen
 import com.example.spike.presentation.ui.shared.screenRegister.RegisterScreen
+import com.example.spike.presentation.ui.shared.screenRegister.RegisterScreenPass
+import com.example.spike.presentation.ui.shared.screenRegister.RegisterViewModel
+import com.example.spike.presentation.ui.shared.screenRegister.ReviewScreen
+import com.example.spike.presentation.ui.shared.screenRegister.registerUser.registerPet.AnimalSelectionScreen
+import com.example.spike.presentation.ui.shared.screenRegister.registerUser.registerPet.RegisterPetScreen
+import com.example.spike.presentation.ui.shared.screenRegister.registerUser.registerPet.RegisterScreenDetails
 import com.example.spike.presentation.ui.shared.screenRegister.registerUser.RegisterScreenUser
+import com.example.spike.presentation.ui.shared.screenRegister.registerUser.RegisterScreenUserDetails
 import com.example.spike.presentation.ui.shared.screenRegister.registerVet.RegisterScreenVet
+import com.example.spike.presentation.ui.shared.screenRegister.registerVet.RegisterScreenVetAddress
 import com.example.spike.presentation.ui.user.appointmentsScreen.AppointmentsScreen
 import com.example.spike.presentation.ui.user.mainScreen.PetCareCatalogueScreen
 import com.example.spike.presentation.ui.user.profileScreen.ProfileScreen
@@ -37,9 +46,32 @@ fun GeneralNavHost(navController: NavHostController) {
         composable(Destination.UserRegister.route) {
             RegisterScreenUser(navController)
         }
-        composable(Destination.VetRegister.route) {
+        composable(Destination.UserDetailsRegister.route) {
+            RegisterScreenUserDetails(navController)
+        }
+        composable(Destination.PassRegister.route) { backStackEntry ->
+            RegisterScreenPass(navController)
+        }
+
+        composable(Destination.VetRegister.route) { backStackEntry ->
             RegisterScreenVet(navController)
         }
+        composable(Destination.VetAddressRegister.route) { backStackEntry ->
+            RegisterScreenVetAddress(navController)
+        }
+       /* composable(Destination.Review.route) { backStackEntry ->
+            ReviewScreen(navController)
+        }*/
+        composable(Destination.AnimalRegister.route) {
+            AnimalSelectionScreen(navController)
+        }
+        composable(Destination.PetRegister.route){
+            RegisterPetScreen(navController)
+        }
+        composable(Destination.PetDetailsRegister.route){
+            RegisterScreenDetails(navController)
+        }
+
 
 //        User
         composable(Destination.UserDestination.VetList.route) {
